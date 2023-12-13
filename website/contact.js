1a9c02c2d2558a3852c6dead3402d625d52070f0
Quinten Schaap <6029404@mborijnland.nl>
2023-12-13 13:05:05 +0100

    alert veranderd

--- website/contact.js
+++ website/contact.js
@@ -1,3 +1,3 @@
 document.getElementById('contactButton').addEventListener('click', function(){
-    alert("0612345678 6029404@mborijnland.nl");
+    alert("telefoonnummer: 0612345678  \nemailadress:  6029404@mborijnland.nl");
 });
