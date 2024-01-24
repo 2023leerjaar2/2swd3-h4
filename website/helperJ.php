740503c491fc28acbdceb904db316444209c3708
Gwuani <6029916@mborijnland.nl>
2024-01-24 13:29:40 +0100

    recept toevoegen doet het

--- website/helperJ.php
+++ website/helperJ.php
@@ -9,7 +9,7 @@ function createRecept($data){
     $vlees_soort = $data["vlees_soort"];
     $afbeelding = $data["afbeelding"];
     $inhoud = $data["inhoud"];
-    $sql ="INSERT INTO producten (titel, land, moeilijkheidsgraad, spicyness, vlees_soort, afbeelding, inhoud)VALUES('$naamRecepten', '$land', '$moeilijkheidsgraad', '$spicyness', '$vlees_soort', '$afbeelding', '$inhoud')";
+    $sql ="INSERT INTO recepten (titel, land, moeilijkheidsgraad, spicyness, vlees_soort, afbeelding, inhoud)VALUES('$titelRecepten', '$land', '$moeilijkheidsgraad', '$spicyness', '$vlees_soort', '$afbeelding', '$inhoud')";
 
     connect()->query($sql);
     echo "recept is toegevoegd!";
