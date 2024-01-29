18d42ac91f0d25f378c393ae51fcc61348d5ed2b
Quinten Schaap <6029404@mborijnland.nl>
2024-01-29 16:51:54 +0100

    css gemaakt

--- website/review.php
+++ website/review.php
@@ -10,7 +10,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/review.css">
+    <link rel="stylesheet" href="css/review.css">
 
     <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
 
@@ -42,7 +42,7 @@
 
 
 
-
+<p>nigger</p>
 
         <section class="invulform">
             <form action="review.php" method="POST">
